a5c4c77240741f830769c1f072aa5f5c118ee45e
Jxd8y <jxvzd@proton.me>
2025-02-02 12:06:38 -0800

    All project file included in project. project is now able to run. some bug fixes and structural changes should happen.

---
+++ HyperDownloadManager/Log/LogRecordDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyperDownloadManager.Dialogs.MessageBoxDialog;
+using System.Xml.Serialization;
+
+namespace HyperDownloadManager.Log
+{
+    [XmlType("Log")]
+    public class LogViewModel
+    {
+        [XmlAttribute("Loglv")]
+        public MessageLevel logLevel { get; set; }
+        [XmlAttribute("Loglc")]
+        public LogSection logsection { get; set; }
+        [XmlText]
+        public string? Log { get; set; }
+        [XmlAttribute("Date")]
+        public DateTime AccureTime { get; set; }
+    }
+}
---
+++ HyperDownloadManager/Log/LogSection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperDownloadManager.Log
+{
+    public enum LogSection
+    {
+        Init,
+        Loader,
+        Download,
+        Container,
+        Utils,
+        Database,
+        Log,
+        Setting
+    }
+}
