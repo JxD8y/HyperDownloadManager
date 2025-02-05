63b7b81b83ebaa27f42bb2cf76886c91c8582e49
Jxd8y <jxvzd@proton.me>
2025-02-05 07:34:46 -0800

    changed structure of IOCore now its only doing basic IO operations not Containing Properties for View

--- HyperDownloadManager/ViewModels/Download/DownloadIO/IOCore.cs
+++ HyperDownloadManager/ViewModels/Download/DownloadIO/IOCore.cs
@@ -4,80 +4,47 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 using System.Windows.Media.Imaging;
 using HyperDownloadManager.Utils;
 using HyperDownloadManager.ViewModels.DataUnit;
-using LiteDB;
-
 namespace HyperDownloadManager.ViewModels.Download.DownloadIO
 {
-    public class IOCore:ViewModel //N: This will change after first success build!
+    public class IOCore
     {
         #region Properties
         private int FSID = -1;
-        [BsonIgnore]
         public IoState IoState { get; set; } = IoState.FileNotOpen;
-        [BsonIgnore]
         public ThrottledStream? fileStream { get; set; }
-        [BsonIgnore]
         public bool isStreamOpen { get; set; } = false;
-        public bool isTempFile { get; set; } = false;
-        public UnitValue fileSize { get; set; } = new UnitValue(0);
-        public string? fileName { get; set; }
-        public string? saveDirectory { get; set; }
-        public string? fileExtension { get; set; }
-        public bool Resumable { get; set; }
-        private long ping = -1;
-        public long Ping { get { return ping; } set { ping = value; OnPropertyChanged(); } }
-        [BsonIgnore]
         public BitmapSource? Icon { get; set; }
-        public Uri? Url { get; set; }
-        [BsonIgnore]
-        public DownloadViewModel? DownloadViewModel { get; set; }
-        public string? fileSavePath
-        {
-            get
-            {
-                if (saveDirectory != null && fileName != null)
-                {
-                    string path = Path.Combine(saveDirectory, fileName);
-                    if (isTempFile)
-                        return Path.Combine(path, $".{IOUtility.TempExtension}");
-                    else
-                        return path;
-                }
-                return "";
-            }
-            set
-            {
-                saveDirectory = Path.GetDirectoryName(value);
-                fileName = Path.GetFileName(value);
-                fileExtension = Path.GetExtension(value);
-            }
-        }
+        public DownloadViewModel model { get; set; } = new DownloadViewModel();
+
         public event EventHandler<EventArgs>? OnFileMaxSize;
         #endregion
+        private long? fileSize = 0;
         public IOCore() { }
-        public IOCore(string savePath, bool TempFile, long fileSize, DownloadViewModel downloadViewModel,IOCore? dataCarrier)
+        public IOCore(DownloadViewModel? viewModel)
         {
-            fileSavePath = savePath;
-            isTempFile = TempFile;
-            DownloadViewModel = downloadViewModel;
-            this.fileSize = UnitConverter.DetectConvert(fileSize);
-            fileName = Path.GetFileName(fileSavePath);
-            fileExtension = Path.GetExtension(fileSavePath);
-            if (dataCarrier != null)
+            if (viewModel is DownloadViewModel)
             {
-                this.Resumable = dataCarrier.Resumable;
-                this.Url = dataCarrier.Url;
+                this.model = viewModel;
+                this.fileSize = this.model.FileSize?.OriginData;
+                if (this.model.ConfigViewModel is ConfigViewModel)
+                    this.model.ConfigViewModel.ConfigUpdated += ConfigViewModel_ConfigUpdated;
             }
-            if (DownloadViewModel.ConfigViewModel != null)
-                DownloadViewModel.ConfigViewModel.ConfigUpdated += ConfigViewModel_ConfigUpdated;
+            else
+            {
+                throw new ArgumentNullException("DownloadViewModel was null");
+            }
         }
 
         private void ConfigViewModel_ConfigUpdated(object? sender, List<object> e)
         {
-            if (e.Where((obj) => { string a = obj.GetType().GetProperties().First().Name; if (a == "MaxFileSize" || a == "SpeedLimit") return true; else return false; }).Count() != 0)
+            if (e.Where((obj) => { string a = obj.GetType().GetProperties().First().Name;
+                if (a == "MaxFileSize" || a == "SpeedLimit") //IO related config properties
+                    return true; 
+                else return false; }).Count() != 0)
             {
                 this.CloseFile();
                 this.OpenFile();
@@ -85,48 +52,58 @@ namespace HyperDownloadManager.ViewModels.Download.DownloadIO
         }
         public void ResetFileData()
         {
-            IOUtility.CloseStream(FSID);
-            File.Delete(fileSavePath);
-            File.Create(fileSavePath).Dispose();
-            this.OpenFile();
-        }
-        public void ResetSupervisor()
-        {
-            if (isStreamOpen)
+            if (this.model?.FileSavePath != null)
             {
-                fileStream?.Flush();
-                fileStream.Position = 0;
+                IOUtility.CloseStream(FSID);
+                File.Delete(this.model.FileSavePath);
+                File.Create(this.model.FileSavePath).Dispose();
+                this.OpenFile();
             }
             else
             {
-                OpenFile();
-                ResetSupervisor();
+                throw new NullReferenceException("File save path in downloadViewModel was null");
             }
         }
-        public void ClearTempFile()
+        public void Reset()
         {
-            if (isTempFile)
+            if (isStreamOpen)
             {
-                if (IoState == IoState.FileOk)
+                if(this.fileStream is ThrottledStream)
                 {
-                    CloseFile();
-                    isTempFile = false;
-                    File.Move(saveDirectory, Path.Combine(saveDirectory, fileName));
+                    this.fileStream.Position = 0L;
+                    this.fileStream?.Flush();
+                }
+                else
+                {
+                    throw new NullReferenceException("fileStream is null");
                 }
             }
             else
             {
-                this.CloseFile();
+                OpenFile();
+                Reset();
+            }
+        }
+        public void ClearTempFile()
+        {
+            this.CloseFile();
+            if (this.model.IsTempFile)
+            {
+                if (IoState == IoState.FileOk && (this.model.FileSavePath != null && this.model.CurrentFileName != null && this.model.CurrentSaveFileDirectory != null))
+                {
+                    File.Move(this.model.FileSavePath, Path.Combine(this.model.CurrentSaveFileDirectory, this.model.CurrentFileName));
+                }
             }
         }
         #region StreamBaseFunctions
         public void OpenFile()
         {
-            if (fileName == null) throw new FileNotFoundException("File not found.");
+            if (this.model.FileSavePath == null)
+                throw new FileNotFoundException("File not found");
             if (!isStreamOpen)
             {
                 ThrottledStream? stream = null;
-                stream = IOUtility.CreateStream(fileSavePath, DownloadViewModel.ConfigViewModel.SpeedLimit, DownloadViewModel.ConfigViewModel.MaxFileSize, FileMode.Open);
+                stream = IOUtility.CreateStream(this.model.FileSavePath, this.model.ConfigViewModel.SpeedLimit, this.model.ConfigViewModel.MaxFileSize, FileMode.Open);
                 if (stream != null)
                 {
                     this.FSID = IOUtility.GetStreamID(stream);
@@ -139,13 +116,13 @@ namespace HyperDownloadManager.ViewModels.Download.DownloadIO
                 {
                     isStreamOpen = false;
                     IoState = IoState.FileNotExist;
-                    throw new FileNotFoundException("File not found.");
+                    throw new FileNotFoundException("fail to open stream on desired file path");
                 }
             }
         }
         public void CloseFile()
         {
-            if (fileStream != null || isStreamOpen)
+            if (fileStream != null && isStreamOpen)
             {
                 IOUtility.CloseStream(FSID);
                 this.isStreamOpen = false;
@@ -157,69 +134,67 @@ namespace HyperDownloadManager.ViewModels.Download.DownloadIO
         #region StreamFunctions
         public void writeToFile(byte[] data, int offset, int count)
         {
-            if (!isStreamOpen)
-            {
+            if (data == null) 
+                throw new ArgumentNullException("data is null");
+            if (!isStreamOpen || fileStream == null)
                 OpenFile();
-                writeToFile(data, offset, count);
-            }
-            if (data == null) throw new ArgumentNullException("data is null");
             else
             {
-                fileStream.Position = offset;
-                fileStream?.Write(data, 0, count);
-                fileStream?.Flush();
-                fileStream.Position = fileStream.Length;
+                try
+                {
+                    fileStream.Position = offset;
+                    fileStream?.Write(data, 0, count);
+                    fileStream?.Flush();
+                    fileStream.Position = fileStream.Length;
+                }
+                catch
+                {
+                    this.CloseFile();
+                    throw;
+                }
             }
         }
         public void writeToFile(byte[] data, int count)
         {
+            if (data == null) 
+                throw new ArgumentNullException("data is null");
             if (!isStreamOpen)
             {
                 OpenFile();
                 writeToFile(data, count);
             }
-            if (data == null) throw new ArgumentNullException("data is null");
             else
             {
-                fileStream?.Write(data, 0, count);
-                fileStream?.Flush();
+                try
+                {
+                    fileStream?.Write(data, 0, count);
+                    fileStream?.Flush();
+                }
+                catch
+                {
+                    this.CloseFile();
+                    throw;
+                }
+                
             }
         }
         public byte[] readFileBytes(int offset, int length)
         {
-            byte[] bytes = new byte[length];
-            fileStream.Position = offset;
-            fileStream?.Read(bytes, 0, length);
-            fileStream.Position = fileStream.Length;
-            return bytes;
-        }
-        #endregion
-        #region StaticMembers
-        public static IOCore? CreateInfoSupervisor(long fileSize, bool resumable, string savePath, Uri url, bool saveTemp)
-        {
-            IOCore viewModel = new IOCore();
-            viewModel.fileSize = UnitConverter.DetectConvert(fileSize);
-            viewModel.Resumable = resumable;
-            if (url != null)
+            if (!isStreamOpen || fileStream == null)
+                OpenFile();
+            try
             {
-                string fileName = Path.GetFileName(url.LocalPath);
-                viewModel.fileSavePath = Path.Combine(savePath, fileName);
-                viewModel.isTempFile = saveTemp;
-                if (string.IsNullOrEmpty(viewModel.fileName))
-                {
-                    viewModel.fileName = url.LocalPath.Replace("\\", "").Replace("/", "");
-                    viewModel.fileExtension = "";
-                }
-                viewModel.Url = url;
-                return viewModel;
+                byte[] bytes = new byte[length];
+                fileStream.Position = offset;
+                fileStream?.Read(bytes, 0, length);
+                fileStream.Position = fileStream.Length;
+                return bytes;
             }
-            return null;
-        }
-        public static bool IsValidInfoCarrier(IOCore? iOSupervisor)
-        {
-            if (iOSupervisor == null)
-                return false;
-            return true;
+            catch
+            {
+                this.CloseFile();
+                throw;
+            }
         }
         #endregion
     }
